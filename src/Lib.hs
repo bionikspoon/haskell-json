e1cfd362f7cd383a00dd1e251a0486000c951c52
Manu Phatak <manu.phatak@procore.com>
2020-01-11 22:14:11 -0800

    Parsing json null, bools, numbers, and strings

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,68 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
+
+import           Data.Char
+import           Control.Applicative
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data JsonValue
+  = JsonNull
+  | JsonBool Bool
+  | JsonNumber Int
+  | JsonString String
+  | JsonArray [JsonValue]
+  | JsonObject [(String, JsonValue)]
+  deriving (Show, Eq)
+
+newtype Parser a = Parser { runParser :: String -> Maybe (String, a)}
+
+instance Functor Parser where
+  fmap fn (Parser p) = Parser $ \input -> do
+    (input', a) <- p input
+    Just (input', fn a)
+
+instance Applicative Parser where
+  pure a = Parser $ \input -> Just (input, a)
+  (Parser p1) <*> (Parser p2) = Parser $ \input -> do
+    (input' , fn) <- p1 input
+    (input'', a ) <- p2 input'
+    Just (input'', fn a)
+
+instance Alternative Parser where
+  empty = Parser $ const Nothing
+  Parser p1 <|> Parser p2 = Parser $ \input -> p1 input <|> p2 input
+
+charP :: Char -> Parser Char
+charP x = Parser fn where
+  fn (y : ys) | y == x = Just (ys, x)
+  fn _                 = Nothing
+
+stringP :: String -> Parser String
+stringP = traverse charP
+
+spanP :: (Char -> Bool) -> Parser String
+spanP fn = notNull $ Parser $ \input ->
+  let (token, input') = span fn input in Just (input', token)
+
+notNull :: Parser [a] -> Parser [a]
+notNull (Parser p) = Parser $ \input -> do
+  s <- p input
+  if null (snd s) then Nothing else Just s
+
+jsonNull :: Parser JsonValue
+jsonNull = JsonNull <$ stringP "null"
+
+jsonBool :: Parser JsonValue
+jsonBool =
+  (JsonBool True <$ stringP "true") <|> (JsonBool False <$ stringP "false")
+
+jsonNumber :: Parser JsonValue
+jsonNumber = f <$> spanP isDigit where f ds = JsonNumber $ read ds
+
+jsonString :: Parser JsonValue
+jsonString = JsonString <$> (charP '"' *> spanP (/= '"') <* charP '"')
+
+jsonValue :: Parser JsonValue
+jsonValue = jsonNull <|> jsonBool <|> jsonNumber <|> jsonString
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,60 @@
+{-# LANGUAGE LambdaCase #-}
+{- HLINT ignore "Redundant do" -}
+{- HLINT ignore "Use isNothing" -}
+
+import           Test.Hspec
+import           Test.QuickCheck
+import           Control.Exception              ( evaluate )
+import           Data.Maybe
+import           Data.Char
+import           Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "Functor" $ do
+    it "can penetrate the Parser with fmap" $ do
+      runParser (fmap pred (charP 'n')) "nice" `shouldBe` Just ("ice", 'm')
+
+  describe "charP" $ do
+    it "parses a single character" $ do
+      runParser (charP 'n') "n" `shouldBe` Just ("", 'n')
+    it "is nothing" $ runParser (charP 'n') "hello" `shouldBe` Nothing
+    it "parses any single character" $ property $ \case
+      ""         -> runParser (charP 'a') [] == Nothing
+      s@(x : xs) -> runParser (charP x) s == Just (xs, x)
+
+  describe "stringP" $ do
+    it "matches a string" $ do
+      runParser (stringP "null") "null" `shouldBe` Just ("", "null")
+    it "returns the remainders" $ do
+      runParser (stringP "null") "null, otherstuff"
+        `shouldBe` Just (", otherstuff", "null")
+    it "is nothing" $ do
+      runParser (stringP "nil") "null" `shouldBe` Nothing
+
+  describe "spanP" $ do
+    it "matches a string with a predicate" $ do
+      runParser (spanP isDigit) "123hello" `shouldBe` Just ("hello", "123")
+
+  describe "jsonNull" $ do
+    it "matches null" $ do
+      runParser jsonNull "null" `shouldBe` Just ("", JsonNull)
+
+  describe "jsonBool" $ do
+    it "matches true" $ do
+      runParser jsonBool "true" `shouldBe` Just ("", JsonBool True)
+    it "matches false" $ do
+      runParser jsonBool "false" `shouldBe` Just ("", JsonBool False)
+
+  describe "jsonNumber" $ do
+    it "matches numbers" $ do
+      runParser jsonNumber "123hello" `shouldBe` Just ("hello", JsonNumber 123)
+    it "isNothing" $ do
+      runParser jsonNumber "hello" `shouldBe` Nothing
+
+  describe "jsonString" $ do
+    it "matches numbers" $ do
+      runParser jsonString "\"hello\"" `shouldBe` Just ("", JsonString "hello")
+    it "isNothing" $ do
+      runParser jsonString "\"hello" `shouldBe` Nothing
+      runParser jsonString "hello\"" `shouldBe` Nothing
